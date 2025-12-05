aca21ac7d2176b8e9236a01a8cda7df5e7149cd6
uczen <uczen@VLADYMIER>
2025-12-05 14:54:11 +0100

    Pop

--- Database/Sensor.cs
+++ Database/Sensor.cs
@@ -3,4 +3,9 @@ public class Sensor
     public int Id { get; private set; }
     public string Type { get; set; }
     public DateTime MeasurementDate { get; set; }
+    private static int counter;
+
+    public Sensor() {
+        counter++;
+    } 
 }
--- Database/User.cs
+++ Database/User.cs
@@ -1,4 +1,11 @@
 public class User {
     public int Id { get; private set; }
     public string Name { get; set; }
+    private static int counter;
+    public User(string name, int id) { 
+        Name = name; 
+        Id = id;
+
+        counter++;
+    }
 }
