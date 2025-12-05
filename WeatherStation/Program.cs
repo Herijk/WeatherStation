3644c39d4ea029b512060a5b43075b90e61c4430
Feranen <127853064+Feranen@users.noreply.github.com>
2025-12-05 13:57:17 +0100

    herijk

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -6,7 +6,19 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        User user = new User();
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        User user4 = new User();
+        User user5 = new User();
+        User user6 = new User();
+        User user7 = new User();
+        User user8 = new User();
+        User user9 = new User();
+        User user10 = new User();
+        User user11 = new User();
+        User user12 = new User();
+        User user13 = new User();
         Sensor sensor = new Sensor();
     }
 }
